4942ab13e7c1c755897bb7fead88434a56b7ba0e
brunomeloesilva <brunomeloesilva@gmail.com>
2020-05-20 16:16:42 -0400

    3.9. Desafio: Validando a descrição

--- src/main/java/bms/brewer/model/Cerveja.java
+++ src/main/java/bms/brewer/model/Cerveja.java
@@ -1,13 +1,29 @@
 package bms.brewer.model;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 public class Cerveja {
 	
 	@NotBlank
 	private String sku;
+	@NotBlank
 	private String nome;
+	@Size(min = 1, max = 50)
+	private String descricao;
 	
+	/**
+	 * @return the descricao
+	 */
+	public String getDescricao() {
+		return descricao;
+	}
+	/**
+	 * @param descricao the descricao to set
+	 */
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
 	/**
 	 * @return the sku
 	 */
