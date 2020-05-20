cc9a919ce904413fb4c7e47c61cf30cbb94ba1d3
brunomeloesilva <brunomeloesilva@gmail.com>
2020-05-20 14:30:02 -0400

    3.7. Introdução a validação do formulário

--- src/main/java/bms/brewer/controller/CervejasController.java
+++ src/main/java/bms/brewer/controller/CervejasController.java
@@ -1,6 +1,9 @@
 package bms.brewer.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -15,9 +18,25 @@ public class CervejasController {
 	}
 	
 	@RequestMapping(value = "/cerveja/novo", method = RequestMethod.POST)
-	public String cadastrar(Cerveja cerveja) {
+	public String cadastrar(@Valid Cerveja cerveja, BindingResult bindingResult) {
+		
+		if(bindingResult.hasErrors()) {
+			System.out.println("Deu pau ao criar o obj cerveja");
+		}else {
 		System.out.println("....Cadastrar: " + cerveja.getSku());
+		}
+		
 		return "cerveja/CadastroCerveja";
 	}
 
 }
+
+/*** Notas de estudo 
+ *
+ * public String cadastrar();
+ *
+ ** Se os dados do post "casarem" com a classe cerveja, é gerado um vinculo para criar o obj é criado automagicamente
+ *
+ ** BindingResult = Recebe o resultado da execucao de criar o obj cerveja pelo post
+ *
+****/
