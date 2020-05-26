d1570fe2bd9d0a1b25e3214a0a2261831b0b6c91
brunomeloesilva <brunomeloesilva@gmail.com>
2020-05-26 19:17:53 -0400

    8.3. Mapeamento da entidade Cerveja

--- src/main/java/bms/brewer/model/Cerveja.java
+++ src/main/java/bms/brewer/model/Cerveja.java
@@ -1,53 +1,169 @@
 package bms.brewer.model;
 
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
+
+@Entity
+@Table(name = "cerveja") // Esta anotação é para mudar o nome da tabela no banco de dados.
 public class Cerveja {
-	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long codigo;
+
 	@NotBlank(message = "SKU é obrigatório")
 	private String sku;
+
 	@NotBlank(message = "Nome é obrigatório")
 	private String nome;
-	@Size(min = 1, max = 50, message = "Deve ter de 1 a 50 caracteres")
+
+	@Size(min = 1, max = 50, message = "O tamanho da descrição deve estar entre 1 e 50")
 	private String descricao;
-	
-	/**
-	 * @return the descricao
-	 */
-	public String getDescricao() {
-		return descricao;
-	}
-	/**
-	 * @param descricao the descricao to set
-	 */
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
-	/**
-	 * @return the sku
-	 */
+
+	private BigDecimal valor;
+
+	@Column(name = "teor_alcoolico")
+	private BigDecimal teorAlcoolico;
+
+	private BigDecimal comissao;
+
+	@Column(name = "quantidade_estoque")
+	private Integer quantidadeEstoque;
+
+	@Enumerated(EnumType.STRING) //EnumType.STRING porque eu quero que vá a descricao para o DB, e não o index.
+	private Origem origem;
+
+	@Enumerated(EnumType.STRING)
+	private Sabor sabor;
+
+	@ManyToOne
+	@JoinColumn(name = "codigo_estilo")//renomeando a fk
+	private Estilo estilo;
+
 	public String getSku() {
 		return sku;
 	}
-	/**
-	 * @param sku the sku to set
-	 */
+
 	public void setSku(String sku) {
 		this.sku = sku;
 	}
-	/**
-	 * @return the nome
-	 */
+
 	public String getNome() {
 		return nome;
 	}
-	/**
-	 * @param nome the nome to set
-	 */
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	
-	
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+
+	public BigDecimal getValor() {
+		return valor;
+	}
+
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+
+	public BigDecimal getTeorAlcoolico() {
+		return teorAlcoolico;
+	}
+
+	public void setTeorAlcoolico(BigDecimal teorAlcoolico) {
+		this.teorAlcoolico = teorAlcoolico;
+	}
+
+	public BigDecimal getComissao() {
+		return comissao;
+	}
+
+	public void setComissao(BigDecimal comissao) {
+		this.comissao = comissao;
+	}
+
+	public Integer getQuantidadeEstoque() {
+		return quantidadeEstoque;
+	}
+
+	public void setQuantidadeEstoque(Integer quantidadeEstoque) {
+		this.quantidadeEstoque = quantidadeEstoque;
+	}
+
+	public Origem getOrigem() {
+		return origem;
+	}
+
+	public void setOrigem(Origem origem) {
+		this.origem = origem;
+	}
+
+	public Sabor getSabor() {
+		return sabor;
+	}
+
+	public void setSabor(Sabor sabor) {
+		this.sabor = sabor;
+	}
+
+	public Estilo getEstilo() {
+		return estilo;
+	}
+
+	public void setEstilo(Estilo estilo) {
+		this.estilo = estilo;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cerveja other = (Cerveja) obj;
+		if (codigo == null) {
+			if (other.codigo != null)
+				return false;
+		} else if (!codigo.equals(other.codigo))
+			return false;
+		return true;
+	}
+
 }
